1d9ef3bb4ff8e9a5242b2e7200e07018738d5771
Alexander Lyakhotskiy <alexlyakhotskiy@gmail.com>
2021-07-21 08:45:56 +0300

    added catch and error

--- src/main.js
+++ src/main.js
@@ -22,19 +22,32 @@ function onInputInject({ target: { value } }) {
 function fetchCountries(value) {
   fetch(`https://restcountries.eu/rest/v2/name/${value}`)
     .then(r => r.json())
-    .then(data => findQuatityCountries(data));
+    .then(data => findQuatityCountries(data))
+    .catch(e => {
+      error({
+        text: `${e}`,
+      });
+    });
 }
 
 function findQuatityCountries(data) {
   if (data.length === 1) {
     renderCountre(data);
+    return;
   }
   if (data.length > 1 && data.length <= 10) {
     renderCountriesList(data);
+    return;
   }
   if (data.length > 10) {
     error({
       text: 'Too many matches found. Please enter a more specific query!',
+    });
+    return;
+  }
+  if (data.status === 404) {
+    error({
+      text: 'Nothing matches found.',
     });
   }
 }
